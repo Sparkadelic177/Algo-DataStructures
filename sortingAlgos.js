bca58038d298eeaa368e0a4d6659b7267d18dba5
Sparky Evangelista <sparkyevangelista@gmail.com>
2021-03-02 07:23:33 -0500

    insertionSort

--- sortingAlgos.js
+++ sortingAlgos.js
@@ -62,6 +62,7 @@ function bubbleSort2(arr) {
 //Compare this item to the next item in the array until you find a smaller number
 //If a smaller number is found, designate that smaller number to be the new "minimum" and continue until the end of the array
 //If the "mimimum" is not the value (index) you initially began with, swap the two values
+//Time complexity worst case is O(n^2)
 function selectionSort(array) {
   if (!array || array.length === 1) return array;
   for (let i = 0; i < array.length; i++) {
@@ -81,4 +82,26 @@ function selectionSort(array) {
   return array;
 }
 
-console.log(selectionSort([32, 3, 14, 63, 12, 33]));
+// console.log(selectionSort([32, 3, 14, 63, 12, 33]));
+
+//Insertion Sort - bulds up the sort by gradually creating a larger left half which is always sorted
+//Pseudocode -
+// * Start by picking the second element in the array
+// * Now compare the second element with the one before it and swap if it and swap if necessary
+// * Continue to the next element and if it is in the correct order, iterate throigh the sorted portion (i.e to the left side) to place lement in the correct place
+// * Repeat until the array is sorted
+
+const insertionSort = (array) => {
+  let j;
+  for (let i = 1; i < array.length; i++) {
+    let currentNum = array[i];
+    for (j = i - 1; j >= 0 && array[j] > currentNum; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = currentNum;
+    console.log(array);
+  }
+  return array;
+};
+
+console.log(insertionSort([32, 3, 14, 63, 12, 33]));
