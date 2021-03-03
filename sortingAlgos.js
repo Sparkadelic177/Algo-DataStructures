2a6539eb4834ce0c216396c166151d1386a17c92
Sparky Evangelista <sparkyevangelista@gmail.com>
2021-03-03 10:33:24 -0500

    merge sort

--- sortingAlgos.js
+++ sortingAlgos.js
@@ -90,7 +90,8 @@ function selectionSort(array) {
 // * Now compare the second element with the one before it and swap if it and swap if necessary
 // * Continue to the next element and if it is in the correct order, iterate throigh the sorted portion (i.e to the left side) to place lement in the correct place
 // * Repeat until the array is sorted
-
+// O(n^2) worst case
+// Good for data that needs to be sorted continosuly , streamed data...
 const insertionSort = (array) => {
   let j;
   for (let i = 1; i < array.length; i++) {
@@ -104,4 +105,69 @@ const insertionSort = (array) => {
   return array;
 };
 
-console.log(insertionSort([32, 3, 14, 63, 12, 33]));
+// console.log(insertionSort([32, 3, 14, 63, 12, 33]));
+
+
+
+//Merge Sort
+//Its a combination of two things - merging and soring 
+// Exploits the fact that arrays of 0 or 1 element are always sorted
+// Works by decomposing an array into samller arrays of 0 or 1 elements, then building up a newly sorted array
+
+//In order to implement mereg sort, it's useful to first implement a function responsible for merging two sorted arrays
+//Given two arrays which are sorted, this helper function should create a new array which is also sorted and consists of all of the elements in the two input arrays 
+//This function should run in O(n + m) time and O(n + m) space and should not modify the parameters passed to it 
+
+
+//Puesdo - 
+//Create an empty array, take a look at the smallest values in each input array 
+//while there are still values we haven't looked at....
+    //if the value in the first array is smaller than the value in the second array, push the value in the first arary into our results and move on to the next value in the first array
+    //if the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
+    //once we exhaust one array, push in all remaining values from the other array
+
+const mergeSort = (arr1, arr2) => {
+    if(!arr1 && !arr2) return []
+    if(!arr1) return arr2
+    if(!arr2) return arr1
+
+    let pointer1 = 0;
+    let pointer2 = 0;
+    let result = []
+    while(pointer1 < arr1.length && pointer2 < arr2.length){
+        let num1 = arr1[pointer1];
+        let num2 = arr2[pointer2]
+        if(num1 < num2){
+            result.push(num1);
+            pointer1++
+        }else{
+            result.push(num2);
+            pointer2++
+        }
+    }
+
+    if(pointer1 !== arr1.length){
+        result.push(...arr1.slice(pointer1));
+    }else if(pointer2 !== arr2.length){
+        result.push(...arr2.slice(pointer2))
+    }
+
+    return result;
+}
+
+// console.log(mergeSort([1], []))
+
+//MergeSort PPseudocode full implementation
+//Break up the array into halves untill you have arrays that are empty or have one element 
+//Once you have smaller sorted arrays, merge those arrays with other sorted arrays untill you are back at the full length of the array
+//once the array has been merged back together, return the merged (and sorted !) array
+
+const merge = (arr) => {
+    if(arr.length === 1) return arr;
+    const half = Math.floor((arr.length) / 2);
+    let left = merge(arr.slice(0,half));
+    let right = merge(arr.slice(half));
+    return mergeSort(left, right);
+}
+
+console.log(merge([325,2,5,1,46,7]))
